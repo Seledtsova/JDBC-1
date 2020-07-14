bf90f9a3ef36252e769b9b19edcab2204b0f540b
liza <liza@DESKTOP-M4PO6CG>
2020-07-14 15:48:24 +0300

    JDBC-1

---
+++ JdbcConnection/src/main/java/com/kciray/play/Main.java
@@ -0,0 +1,47 @@
+package com.kciray.play;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.h2.jdbcx.JdbcDataSource;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {//flaw 1: checked exceptions
+            Connection connection = DriverManager.getConnection("jdbc:h2:mem:test","sa","");
+
+            JdbcDataSource dataSource = new JdbcDataSource();
+
+            Statement statement = connection.createStatement();
+            statement.execute("CREATE TABLE Person(id INTEGER, name VARCHAR)");
+
+            statement.execute("INSERT INTO Person VALUES (0, 'Lily')");
+            statement.execute("INSERT INTO Person VALUES (1, 'Robin')");
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Person");
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("name"));
+            }
+
+            try {
+                statement.execute("CREATE TALE");
+            }catch (SQLException e){//flaw 2: no specific exceptions
+                e.printStackTrace();
+            }
+
+            //flaw 3: redundant close() statements
+            resultSet.close();
+            statement.close();
+            connection.close();
+
+            System.out.println(connection.isClosed());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
